ac1f8ecb3558a4aee35be2a756ef61f37b490421
Brandon <brandonkanute@gmail.com>
2022-03-05 16:08:05 +0300

    Added input forms

---
+++ script.js
@@ -0,0 +1,6 @@
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" , "Sunday"];
+
+let a = "Monday";
+    if (days.includes(a)){
+        console.log("It is available")
+}
