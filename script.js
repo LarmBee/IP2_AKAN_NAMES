93c268232fa6689ba7af09ea5c35da0d2a494c8f
Brandon <brandonkanute@gmail.com>
2022-03-06 18:15:26 +0300

    Getting data from form

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 function validateForm() {
-   let name = document
+   let name = document.querySelector("#firstname")
+  // if (name = " "){
+  //    console.error("Name cannot be empty");
+  //    alert("Name cannot be empty !!")
+  // }
    let gender = document.querySelector("#gender")
-   if (gender ="Male" || "male"){
-      alert("Gender is Male")
-   }
-   else if (gender = "Female" || "female"){
-   alert("Gender is female")
-   }
-   else{
-      alert("Please input valid gender")
-   }
+   //if (gender ="Male" || "male"){
+   //   alert("Gender is Male")
+  // }
+   //else if (gender = "Female" || "female"){
+   //alert("Gender is female")
+   //}
+   //else{
+   //   alert("Please input valid gender")
+   //}
+   let date = document.querySelector("#date")
+   alert(date.value)
+
 }
