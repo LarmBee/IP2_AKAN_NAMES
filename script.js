58fba046d8762b7b21e20b5d8d3151c3369ab1ad
Brandon <brandonkanute@gmail.com>
2022-03-06 22:24:31 +0300

    Male switch implementation

--- script.js
+++ script.js
@@ -5,3 +5,28 @@ function validateForm() {
    result.innerHTML = "Hello " + name.value
    }
    
+if (gender === "Male" || "male"){
+   switch(day){
+      case "Sunday":
+         console.log("Kwasi");
+         break;
+      case "Monday":
+         console.log("Kwadwo")
+         break;
+      case "Tuesday":
+         console.log("Kwabena")
+         break; 
+      case "Wednesday":
+         console.log("Kwaku")
+         break;
+      case "Thursday":
+         console.log("Yaw")
+         break;
+      case "Friday":
+         console.log("Kofi")
+         break;    
+      case "Saturday":
+         console.log("Kwame")
+         break;     
+   }
+}
