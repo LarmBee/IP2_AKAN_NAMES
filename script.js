93c5e3c69c3e16442fe09cc3d783c3c18b0d57da
Brandon <brandonkanute@gmail.com>
2022-03-06 22:28:17 +0300

    Female switch implementation

--- script.js
+++ script.js
@@ -30,3 +30,29 @@ if (gender === "Male" || "male"){
          break;     
    }
 }
+
+if (gender === "Female" || "female"){
+   switch(day){
+      case "Sunday":
+         console.log("Akosua");
+         break;
+      case "Monday":
+         console.log("Adwoa")
+         break;
+      case "Tuesday":
+         console.log("Abenaa")
+         break; 
+      case "Wednesday":
+         console.log("Akua")
+         break;
+      case "Thursday":
+         console.log("Yaa")
+         break;
+      case "Friday":
+         console.log("Afua")
+         break;    
+      case "Saturday":
+         console.log("Ama")
+         break;     
+   }
+}
