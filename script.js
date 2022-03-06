7e3a8b984b996c92aac53f64bdea29d9d5de9b72
Brandon <brandonkanute@gmail.com>
2022-03-06 22:13:35 +0300

    Removed script to impllement switch case

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
 function validateForm() {
    let name = document.querySelector("#firstname")
-  // if (name = " "){
-  //    console.error("Name cannot be empty");
-  //    alert("Name cannot be empty !!")
-  // }
-   let gender = document.querySelector("#gender")
-   //if (gender ="Male" || "male"){
-   //   alert("Gender is Male")
-  // }
-   //else if (gender = "Female" || "female"){
-   //alert("Gender is female")
-   //}
-   //else{
-   //   alert("Please input valid gender")
-   //}
-   let date = document.querySelector("#date")
-   alert(date.value)
-
-}
+   let result =document.querySelector("#result")
+  
+   result.innerHTML = "Hello " + name.value
+   }
+   
