e6169f3f518359710b010a8361e29502c6597f5e
Brandon <brandonkanute@gmail.com>
2022-03-11 11:03:37 +0300

    Change gender to radio

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
+
+
 function validateForm() {
    let name = document.getElementById("firstname").value;
-   let gender = document.getElementById("gender").value;
+   let gender = document.querySelector('input[name="gender"]:checked').value;
    let MM = document.getElementById("MM").value;   
    let DD = document.getElementById("DD").value;
    let CC = document.getElementById("CC").value;
    let YY = document.getElementById("YY").value;
+   d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
 
-   let  d = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) +((26*(MM+1)/10)) + DD ) % 7
    let day = Math.round(d)
    switch(day){
+      case 0 || " ":
+         days="None";
+         break;
       case 1:
          days="Sunday";
          break;
@@ -37,30 +42,26 @@ function validateForm() {
    switch(days){
       case "Sunday":
          Akan =("Kwasi");
-         break;
       case "Monday":
-         Akan=("Kwadwo")
-         break;
+         Akan=("Kwadwo");
       case "Tuesday":
          Akan=("Kwabena")
-         break; 
       case "Wednesday":
          Akan=("Kwaku")
-         break;
       case "Thursday":
          Akan=("Yaw")
-         break;
       case "Friday":
          Akan=("Kofi")
-         break;    
       case "Saturday":
          Akan=("Kwame")
-         break;     
    }
    }
 
    if (gender === "Female" || "female"){
    switch(day){
+      case "None":
+         Akan = ("Wrong input !");
+         break;
       case "Sunday":
          console.log("Akosua");
          break;
@@ -84,10 +85,16 @@ function validateForm() {
          break;     
    }
    }
+   if (name.value == " "){
+      alert("Theres a problem with input!!")
+   }
+   else{
+      greeting.innerHTML=" Hello " + name;
+      result.innerHTML = "You were born on a : " + days;
+      final.innerHTML = "Your Akan Name is :" + Akan;
+   }
    
-   greeting.innerHTML=" Hello " + name;
-   result.innerHTML = "You were born on a : " + days;
-   final.innerHTML = "Your Akan Name is :" + Akan;
 
-} 
+}
+   
 
